b320e1372437ca3e8423a2e3fca07035be63058d
Pratham <shettypratham73@gmail.com>
2025-09-30 04:14:45 +0530

    done with about page want to add some images that changes slowly

--- budgit-frontend/src/pages/About.jsx
+++ budgit-frontend/src/pages/About.jsx
@@ -1,6 +1,23 @@
 function About(){
     return(
-        <p>About Us</p>
+        <div>
+        <h1 className="font-bold underline mt-4 ms-5">About Budgit</h1><br></br>
+        <p className="ms-5 font-semibold hover:underline">Budgit is your smart and simple expense tracking companion.</p>
+        <p className="ms-5 font-semibold hover:underline">Budgit is your smart and simple expense tracking companion.</p><br/>
+        <p className="ms-5 font-semibold hover:underline">We designed it to make managing money stress-free, whether you’re:</p>
+        <p className="ms-5 font-semibold hover:underline"> - 💰 Tracking your personal expenses and budgeting daily needs</p>
+        <p className="ms-5 font-semibold hover:underline"> - 🏠 Splitting rent and bills with your roommates</p>
+        <p className="ms-5 font-semibold hover:underline"> - 🍕 Sharing food costs with friends</p>
+        <p className="ms-5 font-semibold hover:underline"> - ✈️ Keeping track of travel expenses when exploring new places</p><br></br>
+        <p className="ms-5 font-semibold hover:underline">With Budgit, you can:</p>
+        <p className="ms-5 font-semibold hover:underline"> - Add and organize your expenses in just a few clicks</p>
+        <p className="ms-5 font-semibold hover:underline"> - Split costs fairly among friends or groups</p>
+        <p className="ms-5 font-semibold hover:underline"> - View summaries and insights to stay on top of your spending</p>
+        <p className="ms-5 font-semibold hover:underline"> - Keep everything transparent and hassle-free</p><br/>
+        <p className="ms-5 font-semibold hover:underline">Our mission is simple:</p>
+        <p className="ms-5 font-semibold hover:underline">👉 Take the stress out of money management, so you can focus on living life.</p><br></br>
+        <p className="ms-5 font-semibold hover:underline"> - “Budgit — Because every rupee counts.”</p>
+        </div>
     );
 }
 
