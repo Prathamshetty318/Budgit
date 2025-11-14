5e5186bc1665d00f2e09bfafd2685550172d4680
Pratham <shettypratham73@gmail.com>
2025-11-15 04:05:04 +0530

    working on JWT a little issue occured

--- budgit-backend/routes/authRoutes.js
+++ budgit-backend/routes/authRoutes.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import { registerUser,loginUser } from '../controllers/authController.js';
+import { registerUser, loginUser } from '../controllers/authController.js';
+import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 
-router.post('/login',loginUser);
+router.get('/dashboard', verifyToken, (req, res) => {
+    res.json({
+    message: "Welcome user!",
+    user: req.user
+    });
+});
 
 export default router;
