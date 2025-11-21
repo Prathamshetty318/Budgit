871ceebcd8de45b73a88ea6fbc88107a6964c9d8
Pratham <shettypratham73@gmail.com>
2025-11-21 14:37:38 +0530

    craete group page UI also done moving forward with expense page UI

--- budgit-frontend/src/pages/CreateGroup.jsx
+++ budgit-frontend/src/pages/CreateGroup.jsx
@@ -1,8 +1,47 @@
+import { useState } from "react";
+
 function CreateGroup(){
+
+    const [name,setName] = useState("");
+    const [members,setMembers] = useState("");
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        alert(`Group Created`)
+    }
+
     return(
-        <div>
-            <h1 className="font-bold text-2xl">Welcome to Budgit Groups Page</h1>
-            <p className="mt-2">This is a Protected page with only Private access</p>
+        <div className="p-6 max-w-md mx-auto">
+            <h1 className="text-3xl font-bold mb-6">Create a New Group</h1>
+
+            <form
+            onSubmit={handleSubmit}
+            className="bg-white  p-6 rounded-lg shadow space-y-4 hover:shadow-lg">
+                <div>
+                    <label className="font-semibold">Group Name</label>
+                    <input
+                    type="text"
+                    className="bg-white p-2 w-full rounded mt-1"
+                    placeholder="Ex: Goa Trip,Roomates"
+                    value={name}
+                    onChange={(e)=>setName(e.target.value)}/>
+                </div>
+
+                <div>
+                    <label className="font-semibold">Add Members</label>
+                    <input
+                    type="text"
+                    className="bg-white p-2 w-full rounded mt-1"
+                    placeholder="pratham@gmail.com, shetty@gmail.com"
+                    value={members}
+                    onChange={(e)=>setMembers(e.target.value)}/>
+                </div>
+
+                <button className="bg-blue-600 text-white px-4 py-2 rounded w-full hover:bg-blue-700">
+                    Create Group
+                </button>
+            </form>
+
         </div>
     )
 }
