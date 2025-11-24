e22132e1016f8e9513eac179a5c437f48a2339c7
Pratham <shettypratham73@gmail.com>
2025-11-24 18:34:16 +0530

    done with group and groupmembers model for backend

---
+++ budgit-backend/models/groupModel.js
@@ -0,0 +1,25 @@
+import pool from "../config/db.js"
+
+export const createGroup = async(name,description, createdby)=>{
+    const result = await pool.query("Insert into groups(name,descripyion,created_by) VALUES ($1,$2,$3) returning *",
+        [name,description,createdby]);
+        return result.rows[0];
+
+};
+
+export const addMemberToGroup = async(group_Id,userId)=>{
+    const result = await pool.query("Insert into groupmembers(group_id,user_id) values ($1,$2)"
+        ,[group_Id,userId]);
+};
+
+
+export const getGroupByUser = async(user_id)=>{
+    const result = await pool.query(`select g.* from groups g Join groupmembers gm on g.id = group_id
+        where gm.user_id = $1`,[user_id]);
+    return result.rows;
+};
+
+export const getGroupDetails = async(group_Id)=>{
+    const result = await pool.query(`select * from groups where id = $1`,[group_Id]);
+    return result.rows[0];
+}
