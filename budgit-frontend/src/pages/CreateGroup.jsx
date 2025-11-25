4645ff5b3db163987292cf49e6c752b989cbabdc
Pratham <shettypratham73@gmail.com>
2025-11-25 06:39:11 +0530

    done with fetch part for create groups page

--- budgit-frontend/src/pages/CreateGroup.jsx
+++ budgit-frontend/src/pages/CreateGroup.jsx
@@ -4,15 +4,41 @@ import { useNavigate } from "react-router-dom";
 function CreateGroup(){
 
     const [name,setName] = useState("");
-    const [members,setMembers] = useState("");
+    const [description,setDescription] = useState("");
 
     const navigate = useNavigate();
 
-    const handleSubmit=(e)=>{
+    const handleSubmit=async (e)=>{
         e.preventDefault();
-        alert(`Group Created`);
+
+        try{
+            const token=localStorage.getItem("token");
+
+            const res = await fetch("http:localhost:5000/api/groups/create",{
+                method:"Post",
+                headers:{
+                    "content-Type":"application/json",
+                    Authorization:`Bearer${token}`,
+                },
+                body: JSON.stringify({name, description}),
+            });
+
+            const data= await res.json();
+            console.log(data);
+
+            if(res.ok){
+                alert("Group created successfully");
+            }
+            else{
+                alert (data.message||"Failed to create Group");
+            }
+            }catch (error){
+                console.error("Error",error);
+                alert("Something went wrong")
+
+        }
         navigate("/groups");
-    }
+    };
 
     return(
         <div className="p-6 max-w-md mx-auto">
@@ -32,13 +58,13 @@ function CreateGroup(){
                 </div>
 
                 <div>
-                    <label className="font-semibold">Add Members</label>
+                    <label className="font-semibold">Description</label>
                     <input
                     type="text"
                     className="bg-white p-2 w-full rounded mt-1"
-                    placeholder="pratham@gmail.com, shetty@gmail.com"
-                    value={members}
-                    onChange={(e)=>setMembers(e.target.value)}/>
+                    placeholder="Goa Trip"
+                    value={description}
+                    onChange={(e)=>setDescription(e.target.value)}/>
                 </div>
 
                 <button className="bg-blue-600 text-white px-4 py-2 rounded w-full hover:bg-blue-700">
