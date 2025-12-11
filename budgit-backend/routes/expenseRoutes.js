31753daa2902775fbd0d98286f5765441298901f
Pratham <shettypratham73@gmail.com>
2025-12-11 11:20:37 +0530

    donw with router for expense as well

---
+++ budgit-backend/routes/expenseRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { addExpense,getGroupExpenseController,getSingleExpenseController } from "../controllers/expenseController.js";
+import {verifyToken} from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/add",verifyToken,addExpense);  //new exp
+
+router.get("group/:groupId",verifyToken,getGroupExpenseController);  //get all exp from grp
+
+router.get("/:id",verifyToken.getSingleExpenseController);  //get single exp with split
+
+export default router;
+
